f4900e05ab2f0fd77b2ad28d3763f0ed466b3155
Josua Perez <josuap63@gmail.com>
2023-05-24 12:24:19 -0400

    Service Containers PHP Interfaces Registration Bindings

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,11 @@
 
 namespace App\Providers;
 
+use App\Services\MailchimpNewsletter;
+use App\Services\Newsletter;
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\ServiceProvider;
+use MailchimpMarketing\ApiClient;
 use Nette\Utils\Paginator;
 
 class AppServiceProvider extends ServiceProvider
@@ -12,7 +16,14 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        app()->bind(Newsletter::class, function () {
+            $client = (new ApiClient)->setConfig([
+                'apiKey' => config('services.mailchimp.key'),
+                'server' => 'us21'
+            ]);
+
+            return new MailchimpNewsletter($client);
+        });
     }
 
     /**
@@ -20,6 +31,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        Model::unguard();
     }
 }
--- resources/views/components/post-comment.blade.php
+++ resources/views/components/post-comment.blade.php
@@ -1,21 +1,24 @@
-<article class="flex bg-gray-100 border border-gray-200 p-6 rounded-xl space-x-4">
-    <div class="flex-shrink-0">
-        <img src="https://i.pravatar.cc/60" alt="" class="rounded-xl">
-    </div>
+@props(['comment'])
 
-    <div>
-        <header class="mb-4">
-            <h3 class="font-bold">John Cena</h3>
-            <p class="text-xs">
-                Posted
-                <time>8 months ago</time>
+<x-panel class="bg-gray-50">
+    <article class="flex space-x-4">
+        <div class="flex-shrink-0">
+            <img src="https://i.pravatar.cc/60?id={{ $comment->user_id }}" alt="" class="rounded-xl">
+        </div>
+
+        <div>
+            <header class="mb-4">
+                <h3 class="font-bold">{{ $comment->author->username }}</h3>
+                <p class="text-xs">
+                    Posted
+                    <time>{{ $comment->created_at->format('F j, Y, g:i a') }}</time>
+                </p>
+            </header>
+
+            <p>
+                {{ $comment->body }}
             </p>
-        </header>
+        </div>
+    </article>
+</x-panel>
 
-        <p>
-            LoremLoremLoremLoremLoremLoremLorem LoremLore
-            mLoremLoremLoremLoremLoremLoremLoremLoremLoremL
-            oremLoremLoremLLoremLoremLorem
-        </p>
-    </div>
-</article>
